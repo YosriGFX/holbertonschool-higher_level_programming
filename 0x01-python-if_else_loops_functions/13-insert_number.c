0495ab062d25ceae9ecad0ea262cc282e246bdd2
YosriGFX <yosrig1997@gmail.com>
2019-12-10 13:39:22 -0500

    slinked

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * insert_node - prints all elements of a listint_t list
+ * @head: pointer
+ * @number: number to add
+ * Return: addresss
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+listint_t *new;
+listint_t *current;
+
+if (!head)
+return (NULL);
+if (!*head)
+{
+new = add_nodeint_end(head, number);
+return (new);
+}
+current = *head;
+for (; current && current->next; current = current->next)
+{
+if (current->next->n >= number)
+{
+new = malloc(sizeof(listint_t));
+if (!new)
+return (NULL);
+new->n = number;
+new->next = current->next;
+current->next = new;
+return (new);
+}
+}
+new = add_nodeint_end(head, number);
+return (new);
+}
