73c4057892a76207447b5a638985e798dc0c779b
YosriGFX <yosrig1997@gmail.com>
2019-12-10 13:33:33 -0500

    fizbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for a in range (1, 101):
+        if (a % 5 == 0) and (a % 3 == 0):
+            print(end="FizzBuzz ")
+        elif a % 5 == 0:
+            print(end="Buzz ")
+        elif a % 3 == 0:
+            print(end="Fuzz ")
+        else:
+            print(end="{} ".format(a))
