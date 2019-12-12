aac0ce30cc603d086e5c0d2223ddd60faaae4d8c
YosriGFX <yosrig1997@gmail.com>
2019-12-12 07:46:31 -0500

    hello

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 __import__("add_0")
 from add_0 import add
-def _print(a, b, c):
+if __name__ == '__main__':
+    a = 1
+    b = 2
     s = add(a, b)
     print("{} + {} = {}".format(a, b, s))
-if __name__ == '__main__':
-    _print(1, 2, 3)
