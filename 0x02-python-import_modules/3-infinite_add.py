f4741a7a0cf3fa6a60e4b3d26942a423dabbd90b
YosriGFX <YosriG1997@gmail.com>
2019-12-13 21:57:12 +0100

    hello world

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,11 +3,6 @@ from sys import argv
 if __name__ == '__main__':
     lon = len(argv)
     b = 0
-    if lon > 3:
-        for a in range(1, lon):
-            b += int(argv[a])
-    elif lon == 3:
-        b = int(argv[1]) + int(argv[2])
-    elif lon == 2:
-        b = int(argv[1])
+    for a in argv[1:]:
+        b += int(a)
     print("{:d}".format(b))
