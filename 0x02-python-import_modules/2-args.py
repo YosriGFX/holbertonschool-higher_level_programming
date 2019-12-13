1f3fde9a71579bc4f6562057346e6ebc21d49a9a
YosriGFX <yosrig1997@gmail.com>
2019-12-13 21:04:26 +0100

    fixing

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys
-print(len(sys.argv[1:]),"arguments:")
-if len(sys.argv) != 1:
-    a = 1
-    for x in sys.argv[1:]:
-        print(end="{}: {}\n".format(a, x))
-        a += 1
+if __name__ == '__main__':
+    print(len(sys.argv[1:]), "arguments:")
+    if len(sys.argv) != 1:
+        a = 1
+        for x in sys.argv[1:]:
+            print(end="{}: {}\n".format(a, x))
+            a += 1
