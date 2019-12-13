38ab46af5609b746a9e9f76c1ed5a47282b7514e
YosriGFX <yosrig1997@gmail.com>
2019-12-13 17:30:00 -0500

    fixing from mac

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+    from sys import argv
+    x = 0
+    for a in argv:
+        if a != argv[0]:
+            x += int(a)
+    print(x)
