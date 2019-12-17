34aceaf155e24ccfc4b587e3aad550c4ec782f06
YosriGFX <yosrig1997@gmail.com>
2019-12-17 08:11:58 -0500

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix != [[]]:
+        for b in range(len(matrix)):
+            for a in range(len(matrix) - 1):
+                print(end="{} ".format(matrix[b][a]))
+            print("{}".format(matrix[b][a + 1]))
+    else:
+        print()
