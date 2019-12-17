0c2212df91e348182f88cef2d1194beb4bb3270a
YosriGFX <yosrig1997@gmail.com>
2019-12-17 08:34:40 -0500

    checker pass me

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for b in matrix:
-        if b:
-            for a in range(len(b)):
-                print(end="{}".format(b[a]))
-                if a == (len(b) - 1):
-                    print("")
-                else:
-                    print(end=" ")
-        else:
-            print("")
+        for a in range(len(b)):
+            print(end="{}".format(b[a]))
+            if a != (len(b) - 1):
+                print(end=" ")
+        print("")
