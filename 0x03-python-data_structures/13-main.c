68dd2cb3920b7d7ab9097776a783d8375c71efd5
YosriGFX <yosrig1997@gmail.com>
2019-12-17 11:47:08 -0500

    from me

--- 0x03-python-data_structures/13-main.c
+++ 0x03-python-data_structures/13-main.c
@@ -13,6 +13,7 @@ int main(void)
 
     head = NULL;
     add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 1);
     add_nodeint_end(&head, 17);
     add_nodeint_end(&head, 972);
     add_nodeint_end(&head, 50);
@@ -22,6 +23,7 @@ int main(void)
     add_nodeint_end(&head, 50);
     add_nodeint_end(&head, 972);
     add_nodeint_end(&head, 17);
+    add_nodeint_end(&head, 1);
     add_nodeint_end(&head, 1);
     print_listint(head);
 
