1ec38020bcd31ce865518266bb797b85c1735285
YosriGFX <YosriG1997@gmail.com>
2019-12-17 16:14:16 +0000

    me

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,12 +8,27 @@ int is_palindrome(listint_t **head)
 {
 	if (*head)
 	{
-		int count = 0;
+		int count = 0, a, b;
 		listint_t *temp;
+		listint_t *ver;
 		temp = *head;
-		while (temp[count])
+		while (temp)
+		{
+			temp = temp->next;
 			count++;
-		return (count);
+		}
+		temp = *head;
+		ver = *head;
+		for (a = 0; a < (count / 2) - 1; a++)
+		{
+			for (b = 0; b < (count - a); b++)
+				ver = ver->next;
+			temp = temp->next;
+			if (ver->n != temp->n)
+				return (0);
+			ver = *head;
+		}
+		return (1);
 	}
 	else
 		return (1);
