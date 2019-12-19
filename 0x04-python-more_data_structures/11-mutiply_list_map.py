2183144b950e9a985af3b46c5c144aa544773959
YosriGFX <yosrig1997@gmail.com>
2019-12-19 14:04:23 -0500

    mutiply

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    def mul(x):
-        return x * number
+    def mul(x): return x * number
     new_list = list(map(mul, my_list))
     return new_list
