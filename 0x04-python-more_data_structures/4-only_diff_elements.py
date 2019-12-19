8cf5326a3bf64d4a57404414e20d7b20ef39b7bc
YosriGFX <yosrig1997@gmail.com>
2019-12-19 12:46:30 -0500

    pep8

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,9 +2,9 @@
 def only_diff_elements(set_1, set_2):
     set_3 = set()
     for a in set_1:
-        if not a in set_2:
+        if a not in set_2:
             set_3.add(a)
     for a in set_2:
-        if not a in set_3:
+        if a not in set_3:
             set_3.add(a)
     return set_3
