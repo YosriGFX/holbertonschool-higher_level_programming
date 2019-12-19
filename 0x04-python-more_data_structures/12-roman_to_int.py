daf59cafa6a515cc5fce1ba08ee4bccc9b87a3e7
YosriGFX <yosrig1997@gmail.com>
2019-12-19 14:23:38 -0500

    fast

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string:
+    if roman_string.isalpha():
         dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         n = len(roman_string)
         total = dic[roman_string[n - 1]]
