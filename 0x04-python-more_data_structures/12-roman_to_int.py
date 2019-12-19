59ee193774a1e2edc6ee7c382f6aff02e8884973
YosriGFX <yosrig1997@gmail.com>
2019-12-19 14:14:45 -0500

    Roman

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string:
+        dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        n = len(roman_string)
+        total = dic[roman_string[n - 1]]
+        for i in range(n - 1, 0, - 1):
+            current = dic[roman_string[i]]
+            prev = dic[roman_string[i - 1]]
+            total += prev if prev >= current else -prev
+        return total
+    else:
+        return None
