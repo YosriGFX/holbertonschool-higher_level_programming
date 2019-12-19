425c850a6468b8384723c53c74fbd377bfdc1997
YosriGFX <yosrig1997@gmail.com>
2019-12-19 13:00:20 -0500

    maybe

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python
 def number_keys(a_dictionary):
-    s = 0
-    for a in a_dictionary:
-        if a is not None:
-            s += 1
-    return s
+    return len(a_dictionary.keys())
