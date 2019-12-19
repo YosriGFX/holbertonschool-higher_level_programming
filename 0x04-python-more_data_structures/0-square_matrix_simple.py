000edeb0381a01ee42bf3a1e32573aa57137673d
YosriGFX <yosrig1997@gmail.com>
2019-12-19 11:12:57 -0500

    pep8

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
+
 def dub(lists=[]):
     new = []
     for a in lists:
         new.append(a ** 2)
     return new
+
+
 def square_matrix_simple(matrix=[]):
     new = []
     for a in matrix:
