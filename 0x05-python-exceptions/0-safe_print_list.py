4ca86b32f449c741e3293ccff9beadb022386b93
Yosri Ghorbel <yosri@Yosris-MacBook-Pro.local>
2020-01-09 04:32:47 +0100

    from mac

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for i in range(x):
+        try:
+            print(end="{}".format(my_list[i]))
+            i = i + 1
+        except IndexError:
+            break
+    print()
+    return i
