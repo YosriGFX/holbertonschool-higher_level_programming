8b38b5fc0979df3518ca9b89d441dd08f443633a
YosriGFX <YosriG1997@gmail.com>
2020-01-09 17:38:04 +0100

    maybe

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,8 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
+    except (TypeError, NameError):
+        return False
     except ValueError as e:
         print("Exception: {}".format(e))
         return False
