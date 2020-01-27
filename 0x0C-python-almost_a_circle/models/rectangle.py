811ea302e9dc0af394fffc16a82deabe64f1fbc8
YosriGFX <YosriG1997@gmail.com>
2020-01-27 21:21:37 +0100

    Almost a circle 🌍

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -39,18 +39,28 @@ class Rectangle(Base):
                 print(end="#")
             print()
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         '''update function'''
-        if len(args) > 0:
+        if "id" in kwargs:
+            self.id = kwargs["id"]
+        elif len(args) > 0:
             self.id = args[0]
-            if len(args) > 1:
-                self.width = args[1]
-                if len(args) > 2:
-                    self.height = args[2]
-                    if len(args) > 3:
-                        self.x = args[3]
-                        if len(args) > 4:
-                            self.y = args[4]
+        if "width" in kwargs:
+            self.width = kwargs["width"]
+        elif len(args) > 1:
+            self.width = args[1]
+        if "height" in kwargs:
+            self.height = kwargs["height"]
+        elif len(args) > 2:
+            self.height = args[2]
+        if "x" in kwargs:
+            self.x = kwargs["x"]
+        elif len(args) > 3:
+            self.x = args[3]
+        if "y" in kwargs:
+            self.y = kwargs["y"]
+        elif len(args) > 4:
+            self.y = args[4]
 
     @property
     def width(self):
