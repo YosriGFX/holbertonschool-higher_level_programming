eb205b295221b2ef9a2297d369754ba63247c2d7
YosriGFX <YosriG1997@gmail.com>
2020-01-28 00:05:42 +0100

    Almost a circle 🌍

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" square file """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''square Class'''
+    def __init__(self, size, x=0, y=0, id=None):
+        '''___init___ function'''
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        '''Square str'''
+        return ("[Square] (%s) %s/%s - %s" % (
+            self.id,
+            self.x,
+            self.y,
+            self.size,
+            ))
