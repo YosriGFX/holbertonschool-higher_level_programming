32fc78240e9b3fc8b7f9eec058b1b19a5eae36a9
YosriGFX <YosriG1997@gmail.com>
2020-01-28 00:17:07 +0100

    Almost a circle 🌍

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,7 +22,7 @@ class Square(Rectangle):
     @property
     def size(self):
         '''size getter'''
-        return self.height
+        return self.width
 
     @size.setter
     def size(self, value):
