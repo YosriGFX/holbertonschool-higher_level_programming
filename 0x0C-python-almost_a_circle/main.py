04553e0785a77cf651b96d02767750d083f6f943
YosriGFX <YosriG1997@gmail.com>
2020-01-27 20:08:11 +0100

    Almost a circle 🌍

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
 """ 5-main """
 from models.rectangle import Rectangle
-
+from models.base import Base
 if __name__ == "__main__":
-
+    b1 = Base()
+    print(b1.id)
+    b2 = Base()
+    print(b2.id)
+    b3 = Base()
+    print(b3.id)
+    b4 = Base(12)
+    print(b4.id)
+    b5 = Base()
+    print(b5.id)
     r1 = Rectangle(4, 6, 2, 1, 12)
     print(r1)
-
     r2 = Rectangle(5, 5, 1)
     print(r2)
+    r1 = Rectangle(4, 6)
+    r1.display()
+    print("---")
+    r1 = Rectangle(2, 2)
+    r1.display()
+    r1 = Rectangle(3, 2)
+    print(r1.area())
+    r2 = Rectangle(2, 10)
+    print(r2.area())
+    r3 = Rectangle(8, 7, 0, 0, 12)
+    print(r3.area())
+    r1 = Rectangle(2, 3, 2, 2)
+    r1.display()
+    print("---")
+    r2 = Rectangle(4, 4, 8, 5)
+    r2.display()
