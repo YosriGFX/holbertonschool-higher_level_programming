a7d31110786df0cbbc99bb0f5aab732ceda05396
YosriGFX <YosriG1997@gmail.com>
2020-01-28 20:35:42 +0100

    Almost a circle 🌍

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,6 +18,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         '''to_json_string Static'''
-        if list_dictionaries is  None:
-            list_dictionaries  = []
+        if not list_dictionaries:
+            list_dictionaries = []
         return (json.dumps(list_dictionaries))
