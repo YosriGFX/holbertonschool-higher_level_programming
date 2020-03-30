56f609bbc09a9420673b515bbcbd7b64b3f5fa9b
YosriGFX <YosriG1997@gmail.com>
2020-03-30 13:33:20 +0100

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,10 +16,13 @@ if __name__ == '__main__':
     )
     cursor = database.cursor()
     state_name = argv[4]
-    query = 'SELECT * FROM states WHERE name = "{}"'.format(state_name)
+    query = 'SELECT * FROM states WHERE name = "{:s}" ORDER BY id ASC'.format(
+        state_name
+    )
     cursor.execute(query)
     states = cursor.fetchall()
     for state in states:
-        print(state)
+        if state[1] == state_name:
+            print(state)
     cursor.close()
     database.close()
