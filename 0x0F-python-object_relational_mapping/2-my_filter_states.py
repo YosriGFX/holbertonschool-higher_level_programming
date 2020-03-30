4b7a4c7e90a67ae3c970e91fbf4ac69682a25981
YosriGFX <YosriG1997@gmail.com>
2020-03-30 13:28:09 +0100

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''0-select_states.py'''
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    '''init by filename'''
+    database = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset='utf8'
+    )
+    cursor = database.cursor()
+    state_name = argv[4]
+    query = 'SELECT * FROM states WHERE name = "{}"'.format(state_name)
+    cursor.execute(query)
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    database.close()
