d28dc98969ef88d09d1d088e6568b3bb9af938df
YosriGFX <YosriG1997@gmail.com>
2020-04-18 22:49:44 +0100

    [ GP | 6-peak.py ]

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Finds a peak in a list'''
+
+
+def find_peak(list_of_integers):
+    '''Finds a peak in a list'''
+    if not list_of_integers:
+        return
+    length = len(list_of_integers)
+    if length == 1:
+        return list_of_integers[0]
+    elif length == 2:
+        if list_of_integers[0] > list_of_integers[1]:
+            return list_of_integers[0]
+        else:
+            list_of_integers[1]
+    peak = list_of_integers[1]
+    a = 1
+    while a < length:
+        if peak < list_of_integers[a]:
+            peak = list_of_integers[a]
+        a += 1
+    return peak
