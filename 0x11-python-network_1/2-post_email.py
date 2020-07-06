de9fa7850fa9bf5a78b85cdc2b687c5561934ff9
YosriGFX <YosriG1997@gmail.com>
2020-07-06 14:13:29 +0100

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Python script that takes in a URL and an email
+'''
+import urllib.parse as parse
+import urllib.request as request
+from sys import argv
+
+if __name__ == "__main__":
+    mailData = parse.urlencode({'email': argv[2]}).encode('utf-8')
+    ReqData = request.Request(argv[1], mailData)
+    with request.urlopen(ReqData) as Data:
+        print(Data.read().decode('utf-8'))
